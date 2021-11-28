5d85404672a70cf5089f046d96180b55f544344e
Jad Fakhoury <jadfakhoury@hotmail.com>
2021-11-28 11:30:54 +0200

    EF installed and clean up performed

---
+++ TradingJournalAPI/Controllers/TradingAPI.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradingJournalAPI.Controllers
+{
+    public class TradingAPI : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
