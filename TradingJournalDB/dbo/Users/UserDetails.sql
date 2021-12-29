49457c2185e9c2648ee7d124eab886d6e94e63b7
Jad Fakhoury <jadfakhoury@hotmail.com>
2021-12-29 17:38:15 +0200

    edited the excel sheet for the db structure

--- TradingJournalDB/dbo/Users/UserDetails.sql
+++ TradingJournalDB/dbo/Users/UserDetails.sql
@@ -2,9 +2,9 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [UserId] INT NOT NULL, 
+    [CountryId] INT NOT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Lastname] NVARCHAR(50) NOT NULL, 
-    [CountryId] INT NOT NULL, 
-    [Mobile] NVARCHAR(50) NOT NULL, 
+    
     CONSTRAINT [FK_UserDetails_Country] FOREIGN KEY (CountryId) REFERENCES Country(Id), 
 )
